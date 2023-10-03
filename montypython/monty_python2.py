982ca997a390d5bc469674d92abfc5ea74ea77c9
rodriguezmarkd <rodriguezmarkd@gmail.com>
2023-10-03 18:22:39 +0000

    While looping

--- montypython/monty_python2.py
+++ montypython/monty_python2.py
@@ -6,6 +6,7 @@ while True:
     round = round + 1
     print('Finish the movie title, "Monty Python\'s The Life of ______"')
     answer = input("Your guess--> ")
+    answer = answer.capitalize()
 
     if answer == 'Brian':
         print('Correct')
