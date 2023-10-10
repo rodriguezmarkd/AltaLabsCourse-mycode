84331ede5ba8614a6bbd65310fc9e57debda6d95
rodriguezmarkd <rodriguezmarkd@gmail.com>
2023-10-10 21:25:10 +0000

    Project 2 work

--- project2/main.py
+++ project2/main.py
@@ -2,8 +2,12 @@
 
 from flask import Flask
 from flask import render_template
+from flask import redirect
+from flask import url_for
+from flask import request
 
-
+app = Flask(__name__)
+'''
 @app.route("/configurator/")
 def gatherparams():
     try:
@@ -14,7 +18,24 @@ def gatherparams():
         return render_template("baseIOS.conf.j2", **params)
     except Exception as err:
         return f"Error at {err}"
+'''
+@app.route("/success/<hn>?<ia>")
+def success(hn, ia):
+    return f"Welcome {hn},\n {ia}"
 
+@app.route("/")
+def start():
+    return render_template("homepage.html")
+
+@app.route("/params", methods = ["POST"])
+def params():
+    if request.method == "POST":
+        if request.form.get("host"):
+            hostname = request.form.get("host")
+    if request.method == "POST":
+        if request.form.get("ip"):
+            ipadd = request.form.get("ip")
+    return redirect(url_for("success", hn = hostname, ia = ipadd))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=2224)
