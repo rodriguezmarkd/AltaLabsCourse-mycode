3feb2854a8fcc9ceb60570c67e9ff6a5be202863
rodriguezmarkd <rodriguezmarkd@gmail.com>
2023-10-13 01:52:46 +0000

    Updating Project 2

--- project2/main.py
+++ project2/main.py
@@ -5,26 +5,24 @@ from flask import render_template
 from flask import redirect
 from flask import url_for
 from flask import request
+import paramiko
+import os
 
 app = Flask(__name__)
 configparams = {}
-'''
-@app.route("/configurator/")
-def gatherparams():
-    try:
-        params={}
-        params['hostname'] = request.args.get('hostname')
 
-
-        return render_template("baseIOS.conf.j2", **params)
-    except Exception as err:
-        return f"Error at {err}"
-'''
 @app.route("/success/<hn>?<ia>")
 def success(hn, ia):
     print(configparams)
-    return render_template("iosconfig.txt", **configparams)
-#    return f"Welcome {hn},\n {ia}"
+    with open("tempconfig","w") as configfile:
+        print(render_template("iosconfig.conf.j2", **configparams), file=configfile)
+    h = paramiko.Transport("10.10.2.3", 22)
+    h.connect(username="bender", password="alta3")
+    sftp = paramiko.SFTPClient.from_transport(h)
+    sftp.put("tempconfig", "/home/bender/config")
+    sftp.close()
+    h.close()
+    return render_template("iosconfig.html", **configparams)
 
 @app.route("/")
 def start():
@@ -36,14 +34,12 @@ def params():
         if request.form.get("host"):
             hostname = request.form.get("host")
             configparams['hostname'] = request.form.get("host")
-    if request.method == "POST":
-        if request.form.get("ip"):
             ipadd = request.form.get("ip")
             configparams['ip'] = request.form.get("ip")
-            configparams['domain'] = "domain"
-            configparams['nameserver'] = "nameserver"
-            configparams['subnet'] = "subnet"
-            configparams['gateway'] = "gateway"
+            configparams['domain'] = request.form.get("domain")
+            configparams['nameserver'] = request.form.get("nameserver")
+            configparams['subnet'] = request.form.get("subnet")
+            configparams['gateway'] = request.form.get("gateway")
     return redirect(url_for("success", hn = hostname, ia = ipadd))
 
 if __name__ == "__main__":
