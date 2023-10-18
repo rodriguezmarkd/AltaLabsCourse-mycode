c5913487fd63733956ddadad8910a26fecc20eef
rodriguezmarkd <rodriguezmarkd@gmail.com>
2023-10-18 16:46:57 +0000

    Project Updates

--- NetworkAutomationProject/main.py
+++ NetworkAutomationProject/main.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python3
 
 import yaml
-from yaml import load
-from yaml import CLoader as Loader
+import subprocess
 
-NETWORK_TOPOLOGY_DICT = {}
-def main():
-    #reader = open('network_topology.yml' ,'r')
-    #topology = yaml.load(reader)
-    #for key, value in topology.items():
-    #    print(key + " : " + str(value))
+def populate_dict():
+    topology = {}
     with open('network_topology.yml', 'r') as reader:
-        NETWORK_TOPOLOGY_DICT = yaml.load(reader, Loader=yaml.FullLoader)
+        topology = yaml.load(reader, Loader=yaml.FullLoader)
+    return topology
+    
 
-    print(NETWORK_TOPOLOGY_DICT)
+def build_network(topology):
+    for key, value in topology['routers'].items():
+        print(key, value)
+        print(key)
+        i = 0
+        if key == 'routers':
+            subprocess.call(['sudo','ip','netns','add',topology['routers'][i]['name']])
+            i = i + 1
+    print(topology.keys())
+    print(topology['routers'][0]['name'])
+    print(topology['routers'][1]['name'])
+    print(topology['routers'][2]['name'])
+def main():
+    network_topology = populate_dict()
+    build_network(network_topology)
+    #print(network_topology)
 
 if __name__ == '__main__':
     main()
